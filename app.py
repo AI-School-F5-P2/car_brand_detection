6bf0451dac5066747e97a16bb90777278e7a38a9
ali-rosano <alicia.rosano@gmail.com>
2023-12-20 13:36:45 +0100

    feat(app): add new features for improved functionality

--- app.py
+++ app.py
@@ -1,41 +1,105 @@
 import requests
+from pathlib import Path
 import streamlit as st
 from PIL import Image
+import subprocess
+import os
 from io import BytesIO
-import tempfile
-from ultralytics import YOLO
-import cv2
 
-# Load a pre-trained YOLOv8n model
-model = YOLO('best.pt')
-names = model.model.names
+def get_subdirs(b='.'):
+    '''
+    Returns all sub-directories in a specific Path
+    '''
+    result = []
+    for d in os.listdir(b):
+        bd = os.path.join(b, d)
+        if os.path.isdir(bd):
+            result.append(bd)
+    return result
 
-st.title('Predicción de YOLOv8 desde URL')
+def get_detection_folder():
+    '''
+    Returns the latest folder in a runs\detect
+    '''
+    return max(get_subdirs(os.path.join('runs', 'detect')), key=os.path.getmtime)
 
-# Entrada para la URL de la imagen
-url = st.text_input('Ingresa la URL de la imagen')
-if url:
-    try:
+def check_folders():
+    paths = {
+        'data_path': 'data',
+        'images_path': 'data/images',
+        'videos_path': 'data/videos'
+    }
+    # Check whether the specified path exists or not
+    notExist = list(({file_type: path for (file_type, path) in paths.items() if not os.path.exists(path)}).values())
 
-        # Realiza la predicción con el modelo
-        results = model(url, show=True, conf=0.5, save=True)
+    if notExist:
+        st.warning(f'Folders {notExist} do not exist. Creating now')
+        # Create a new directory because it does not exist
+        for folder in notExist:
+            os.makedirs(folder)
+            st.write(f"The new directory {folder} is created!")
 
-        # Procesa las detecciones
-        boxes = results[0].boxes.xywh.cpu()
-        clss = results[0].boxes.cls.cpu().tolist()
-        confs = results[0].boxes.conf.float().cpu().tolist()
+# This will check if we have all the folders to save our files for inference
+check_folders()
 
-        # Lee la imagen desde la URL
-        image = Image.open(BytesIO(requests.get(url).content))
-        st.image(image, caption='Imagen cargada', use_column_width=True)
+is_valid = False
 
-        # Muestra los resultados de la predicción
-        st.write('Resultados de la predicción:')
-        for box, cls, conf in zip(boxes, clss, confs):
-            st.write(f"Clase: {names[int(cls)]}, Confianza: {conf}, Caja: {box}")
-    except Exception as e:
-        st.error(f'Ocurrió un error al cargar o procesar la imagen: {e}')
+if __name__ == '__main__':
+    st.title('Car Brand Detection')
 
+    source = ("Upload Image", "Image URL")
+    source_index = st.sidebar.selectbox("Select Input type", range(len(source)), format_func=lambda x: source[x])
 
+    if source_index == 0:
+        uploaded_file = st.sidebar.file_uploader("Load File", type=['png', 'jpeg', 'jpg'])
+        if uploaded_file is not None:
+            is_valid = True
+            with st.spinner(text='Loading...'):
+                st.sidebar.image(uploaded_file, caption='Imagen cargada')
+                picture = Image.open(uploaded_file)
+                picture = picture.save(f'data/images/{uploaded_file.name}')
+                source = f'data/images/{uploaded_file.name}'
+        else:
+            is_valid = False
+    else:
+        url = st.text_input('Enter Image URL')
+        if url:
+            try:
+                image = Image.open(BytesIO(requests.get(url).content))
+                is_valid = True
+                with st.spinner(text='Loading...'):
+                    st.sidebar.image(image, caption='Imagen cargada')
 
+                    # Guarda la imagen en una carpeta local para su posterior uso
+                    image_filename = url.split("/")[-1]  # Obtiene el nombre de archivo de la URL
+                    local_image_path = os.path.join("data", "images", image_filename)
 
+                    # Guarda la imagen descargada localmente
+                    image.save(local_image_path)
+
+                    # Establece la ruta de la imagen como la fuente (source)
+                    source = local_image_path
+            except Exception as e:
+                st.error(f"Ocurrió un error al cargar la imagen desde la URL: {e}")
+                is_valid = False
+
+    if is_valid:
+        print('valid')
+        if st.button('Detect'):
+            with st.spinner(text='Running detection...'):
+                process = subprocess.run(['yolo', 'task=detect', 'mode=predict', 'model=best.pt', 'conf=0.5', 'source={}'.format(source)],
+                                     capture_output=True, universal_newlines=True)
+
+            if process.stderr:
+                st.error(process.stderr)
+            else:
+                st.success("Detection completed successfully!")
+
+            with st.spinner(text='Preparing Image'):
+                for file in os.listdir(get_detection_folder()):
+                    # Mostrar imágenes desde la carpeta de detección
+                    file_path = str(Path(get_detection_folder()) / file)
+                    if file.endswith(('.png', '.jpg', '.jpeg')):
+                        st.image(file_path)
+
+                st.snow()
